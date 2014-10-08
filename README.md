5477efa2479f3bb2a9d4e13d3ea4675af8321995
Jacek Kowalczyk <j.kowalczyk@clearcode.cc>
2014-10-08 13:00:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,188 @@
+Clearcode chart directive
+=========
+
+Adds chart using highcharts.
+
+Installation
+--------------
+TODO
+
+
+Usage
+------
+
+Add ``` cc.chart.directive``` module to your app module list :
+
+
+```
+angular
+    .module('yourAwesomeApp', [
+        'cc.chart.directive'
+    ]);
+```
+and you are ready to go!
+
+How to use :
+
+
+```
+    <chart data-input="data" data-config="chartConfig" />
+```
+
+
+And a little example, in controller:
+
+```
+   var config = {
+    generateSeries: function () {
+      return {
+        series: [{
+          name: 'TEST 1',
+          unit: 'a',
+          toTotal: true,
+          data: [1,4,51,12,4,63,124,56,91,21],
+          type: 'column',
+          color: '#3ab3dc',
+          pointInterval: 24 * 3600 * 1000
+        },
+        {
+          name: 'TEST 2',
+          unit: 'a',
+          toTotal: true,
+          data: [41,51,12,92,74,49,18,21,53,2],
+          type: 'column',
+          color: '#e43d5f',
+          pointInterval: 24 * 3600 * 1000
+        },
+        {
+          name: 'TEST 3',
+          unit: 'a',
+          data: [54,1,13,12,53,1,14,53,11,111],
+          type: 'line',
+          color: '#a8bdc4',
+          dashStyle: 'ShortDash',
+          pointInterval: 24 * 3600 * 1000,
+        }]
+      };
+    },
+    chart: {
+      chart: {
+        plotBorderColor: '#e0e0de',
+        plotBorderWidth: 1
+      },
+      xAxis: {
+        type: 'datetime',
+        gridLineColor: '#e0e0de',
+        gridLineWidth: 1,
+        gridLineDashStyle: 'dash',
+        tickLength: 0,
+        lineWidth: 0
+      },
+      yAxis: [
+        {
+          title: '',
+          endOnTick: true,
+          labels: {
+            formatter: function () {
+              return numeral(this.value).format('0a');
+            }
+          },
+          gridLineColor: '#e0e0de',
+          gridLineDashStyle: 'dash',
+          reversedStacks: false
+        }
+      ],
+      legend: {
+        itemStyle: {
+          color: '#3a3e40',
+          fontWeight: 'normal'
+        }
+      },
+      plotOptions: {
+        series: {
+          pointStart: Date.UTC(2014,0,1),
+          type: 'datetime'
+        },
+        column: {
+          stacking: 'normal',
+          pointPadding: 0.205
+        },
+        line: {
+          marker: {
+            enabled: false,
+            states: {
+              hover: {
+                enabled: false
+              }
+            }
+          },
+          states: {
+            hover: {
+              lineWidth: 2
+            }
+          }
+        }
+      },
+      tooltip: {
+        formatter: function () {
+          var total = 0,
+            stringArray = [];
+
+          stringArray.push('<ul>');
+
+          angular.forEach(this.points, function(point) {
+            stringArray.push('<li><span class="label">' + point.series.name + '</span><br/><span class="value">' +
+              numeral(point.y).format('0' + point.series.options.unit) + '</span></li>');
+            if (point.series.options.toTotal) {
+              total += point.y;
+            }
+          });
+
+          stringArray.push(
+            '<li><span class="label">TOTAL</span><br/><span class="value">' +
+            numeral(total).format('0a') + '</span></li>'
+          );
+
+          // swap the last and second to last element
+          // not very elegant but hey, it works
+
+          (function swap (array) {
+            var buff = array[array.length - 1];
+            array[array.length - 1] = array[array.length - 2];
+            array[array.length - 2] = buff;
+          })(stringArray);
+
+          stringArray.push('</ul>');
+
+          return stringArray.join('');
+        }
+      }
+    }
+  };
+    $scope.data = config.generateSeries();
+    $scope.chartConfig = config.chart;
+```
+
+and in view:
+
+```
+    <chart data-input="data" data-config="chartConfig" />
+```
+
+Result:
+
+![alt text](http://i.imgur.com/acu5yva.png "Logo Title Text 1")
+
+
+
+Author
+------
+
+Witold Galecki
+
+
+License
+----
+
+LGPL
+
---
+++ script/directives/chart.js
@@ -0,0 +1,149 @@
+/*
+
+    Copyright (C) 2012-2013 by Clearcode <http://clearcode.cc>
+    and associates (see AUTHORS).
+
+    This file is part of cc-chart-directive.
+
+    cc-chart-directive is free software: you can redistribute it and/or modify
+    it under the terms of the Lesser GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    cc-chart-directive is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with cc-chart-directive.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+(function (angular, $, Highcharts) {
+
+  'use strict';
+
+  var utils = {
+    processResponse: function (chart, data) {
+      // check if there are multiple series in the response
+      if (data.series && angular.isArray(data.series)) {
+        // check if the chart is empty and if it is,
+        // just add series from the response
+        if (chart.series.length === 0) {
+          data.series.forEach(function (serie) {
+            chart.addSeries(serie);
+          });
+        } else {
+          // if response method is set to 'replace'
+          // remove all series from the chart
+          if (data.method === 'replace' || !data.method) {
+            while(chart.series.length > 0) {
+              chart.series[0].remove();
+            }
+          }
+          // iterate over the supplied series and alter the chart
+          angular.forEach(data.series, function (serie, index) {
+            if (data.method === 'shift') {
+              chart.series[index].addPoint(serie.data[serie.data.length - 1], true, true);
+            } else if (data.method === 'update'){
+              chart.series[index].setData(serie.data, true);
+            } else if (data.method === 'replace' || !data.method) {
+              chart.addSeries(serie, true, true);
+            }
+          });
+        }
+      // if there is just one series in the response
+      } else {
+        if (chart.series.length === 0) {
+          chart.addSeries(data);
+        } else {
+          if (data.method === 'shift') {
+            chart.series[0].addPoint(data.data[data.data.length - 1], true, true);
+          } else if (data.method === 'update') {
+            chart.series[0].setData(data.data, true);
+          } else if (data.method === 'replace' || !data.method){
+            chart.series[0].remove();
+            chart.addSeries(data, true, true);
+          }
+        }
+      }
+    }
+  };
+
+  angular.module('cc.chart.directive')
+    .directive('chart', function () {
+      return {
+        restrict: 'E',
+        template: '<div class="chart"></div>',
+        replace: true,
+        scope: {
+          config: '=',
+          data: '=input'
+        },
+        link: function (scope, element) {
+
+          var chart,
+            defaults = {
+              tooltip: {
+                shared: true,
+                useHTML: true,
+                borderWidth: 0,
+                shadow: false,
+                backgroundColor: null,
+                style: {
+                  padding: 0
+                },
+                followPointer: false,
+              },
+              legend: {
+                layout: 'horizontal',
+                align: 'left',
+                padding: 0,
+                x: 40,
+                y: 15,
+                verticalAlign: 'top',
+                floating: true,
+                backgroundColor: 'transparent',
+                borderWidth: 0,
+                symbolHeight: 2
+              },
+              chart: {
+                margin: [50, 50, 50, 50],
+                renderTo: element[0]
+              },
+              credits : {
+                enabled: false
+              },
+              title: {
+                text: ''
+              },
+              plotOptions: {
+                column: {
+                  borderRadius: 2,
+                  borderWidth: 0
+                },
+                spline: {
+                  marker: {
+                    symbol: 'circle'
+                  }
+                }
+              }
+            };
+
+          scope.$watch('config', function (config) {
+            if (config && !chart) {
+              chart = new Highcharts.Chart($.extend(true, {}, defaults, config));
+            }
+          });
+
+          scope.$watch('data', function (data) {
+            if (data && chart) {
+              utils.processResponse(chart, data);
+            }
+          });
+
+        }
+      };
+    });
+
+})(window.angular, window.jQuery, window.Highcharts);
